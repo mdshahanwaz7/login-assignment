32a189a3082247eb47d7ab0103df17d7b6cb2aae
mdshahanwaz7 <hasmatikhatoon77@gmail.com>
2026-01-10 16:22:07 +0530

    updated frontend

--- Home.jsx
+++ Home.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export const Home = () => {
   return (
-    <div>Welcome to Home</div>
+    <div className=' bg-gray-100  flex justify-center items-center min-h-screen'>
+    <div className='bg-indigo-900 flex flex-col justify-center items-center rounded-md shadow-md hover:shadow-xl  hover:border-cyan-300 border border-transparent  hover:ring-1 hover:ring-cyan-300 transition focus:border-cyan-300   size-[300px]'>
+      <h1 className=' text-white font-bold text-xl  '> Welcome to Home!</h1>
+    </div>
+    </div>
   )
 }
